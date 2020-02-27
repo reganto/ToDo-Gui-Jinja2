7b1902886513b225678b92b4493c732b80e735b0
reganto <rreganto@gmail.com>
2020-02-27 17:43:51 +0330

    add Job model

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://reganto:540689m$@localhost:3306/todo"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
+
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.Text)
+    completed = db.Column(db.Boolean, default=False)
 
 
 @app.route("/")
