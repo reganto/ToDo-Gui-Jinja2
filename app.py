4ce5d403970ed6de510d02863af89331ffff26f3
reganto <rreganto@gmail.com>
2020-02-28 16:09:06 +0330

    add new job completed

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import os
+
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField, BooleanField
 from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://reganto:540689m$@localhost:3306/todo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.urandom(16)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
@@ -21,6 +24,31 @@ class Job(db.Model):
     completed = db.Column(db.Boolean, default=False)
 
 
+class NewForm(FlaskForm):
+    title = StringField("Title", validators=[InputRequired()])
+    text = TextAreaField("Text")
+    completed = BooleanField("Completed")
+
+
 @app.route("/")
 def index():
     return render_template("new.html")
+
+
+@app.route("/new/", methods=["GET", "POST"])
+def new():
+    form = NewForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        completed = form.completed.data
+
+        job = Job(title=title, text=text, completed=completed)
+        db.session.add(job)
+        db.session.commit()
+        
+        return "Job added"
+    
+    return render_template("new.html", form=form)
+    
